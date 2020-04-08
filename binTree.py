dcb3224f4d0b95ea3aa054b400c650bb61f2f9eb
lebranchu mehdi <mehdi.lebranchu@gmail.com>
2020-04-08 16:35:30 +0200

    Arbre

---
+++ binTree.py
@@ -0,0 +1,26 @@
+from ex1 import *
+class binaryTree:
+    def __init__(self,r):
+        self.__root = r
+    def getRoot(self):
+        return self.__root
+    def isRoot(self,n):
+        if n.getVal() == 12:
+            return True
+        else :
+            return False
+
+
+if __name__ == '__main__':
+    n6 = node(6,None,None)
+    n7 = node(21,None,None)
+    n8 = node(18,None,None)
+    n9 = node(3,None,None)
+    n5 = node(4,None,n9)
+    n4 = node(19,n7,n8)
+    n3 = node(17,n4,None)
+    n2 = node(5,n6,n5)
+    n1 = node(12, n3, n2)
+    b1 = binaryTree(n1)
+    print(b1.isRoot(n2))
+
---
+++ ex1.py
@@ -0,0 +1,29 @@
+class node:
+    def __init__(self,a,b,c):
+        self.__val = a
+        self.__right = b
+        self.__left = c
+    def getVal(self):
+        return self.__val
+    def getRight(self):
+        return self.__right.getVal()
+    def getLeft(self):
+        return self.__left.getVal()
+    def setRight(self,a):
+        self.__right = a
+    def setLeft(self,a):
+        self.__left = a
+
+
+if __name__ == '__main__':
+    n6 = node(6,None,None)
+    n7 = node(21,None,None)
+    n8 = node(18,None,None)
+    n9 = node(3,None,None)
+    n5 = node(4,None,n9)
+    n4 = node(19,n7,n8)
+    n3 = node(17,n4,None)
+    n2 = node(5,n6,n5)
+    n1 = node(12, n3, n2)
+
+    print(n1.getLeft())
